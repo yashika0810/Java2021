3dfb0d792420efd2f12372410ea6b2bc0f31b262
yashika0810 <48493052+yashika0810@users.noreply.github.com>
2021-01-05 15:55:59 -0500

    Update Demo.java
    set value of a to 200

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Consultadd");
-        int a=22;
+        int a=200;
         int b=26;
         System.out.println("result" + (a+b));
     }
