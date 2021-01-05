734dfb543cbeec7d9f4bd19b25f1265c34a28782
yashika0810 <48493052+yashika0810@users.noreply.github.com>
2021-01-05 15:54:16 -0500

    Update Demo.java
    sum operation

--- Demo.java
+++ Demo.java
@@ -1,5 +1,8 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Consultadd");
+        int a=22;
+        int b=26;
+        System.out.println("result" + (a+b));
     }
 }
